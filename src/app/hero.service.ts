f4f7a8dc06413f907985f4420f7c943d2b02d011
Oleg Sokolovsky <Martiniks.so@gmail.com>
2021-03-30 17:00:02 +0300

    Services step two

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
@@ -7,9 +8,10 @@ import { HEROES } from './mock-heroes';
 })
 export class HeroService {
 
-  constructor() { }
+  	constructor() { }
 
-  getHeroes(): Hero[] {
-  	return HEROES;
-	}
+  	getHeroes(): Observable<Hero[]> {
+    	const heroes = of(HEROES);
+	    return heroes;
+  	}	
 }
